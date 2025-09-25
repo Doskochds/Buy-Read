3eb3275988da206238cb7c868fb164e591c5afb5
Doskochds <darkstrik2000@gmail.com>
2025-09-25 14:43:25 +0300

    1sasas
    1XAA

---
+++ DoskochKursova/DoskochKursova/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoskochKursova.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
